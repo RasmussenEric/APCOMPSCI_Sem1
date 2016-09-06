ad67a3e886ea573ca87913c5ecba3fdd486db339
RasmussenEric <ericrasmussen17@yahoo.com>
2016-09-06 11:00:01 -0700

    Added Files

---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,28 @@
+public class ASCII2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                      __---__ ");
+		System.out.println("                   _-       _--______ ");
+		System.out.println("              __--( /     \\ )XXXXXXXXXXXXX_ ");
+		System.out.println("            --XXX(   O   O  )XXXXXXXXXXXXXXX- ");
+		System.out.println("            /XXX(       U     )        XXXXXXX\\ ");
+		System.out.println("          /XXXXX(              )--_  XXXXXXXXXXX\\ ");
+		System.out.println("         /XXXXX/ (      O     )   XXXXXX   \\XXXXX\\ ");
+		System.out.println("         XXXXX/   /            XXXXXX   \\__ \\XXXXX----");
+		System.out.println("         XXXXXX__/          XXXXXX         \\__----  -");
+		System.out.println(" ---___  XXX__/          XXXXXX      \\__         ---");
+		System.out.println("  --  --__/   ___/\\  XXXXXX            /  ___---=");
+		System.out.println("    -_    ___/    XXXXXX              '--- XXXXXX");
+		System.out.println("      --\\/XXX\\ XXXXXX                      /XXXXX");
+		System.out.println("        \\XXXXXXXXX                        /XXXXX/");
+		System.out.println("         \\XXXXXX                        _/XXXXX/");
+		System.out.println("           \\XXXXX--__/              __-- XXXX/");
+		System.out.println("            --XXXXXXX---------------  XXXXX--");
+		System.out.println("               \\XXXXXXXXXXXXXXXXXXXXXXXX-");
+		System.out.println("                 --XXXXXXXXXXXXXXXXXX-" );
+		System.out.println("           * * * * * who ya gonna call? * * * * *");
+		System.out.println("                 * * * ghostbusters!! * * * ");
+	}
+}
+		
