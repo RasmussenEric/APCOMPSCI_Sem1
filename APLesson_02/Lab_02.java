10eee949f906732b31e758e9daf329920d143ca9
RasmussenEric <ericrasmussen17@yahoo.com>
2016-09-08 10:32:40 -0700

    added lesson

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,24 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int num1 = 2;
+		int num2 = 4;
+		
+		System.out.println(num1 + " multiplied by " + num2 + " is " + num1 * num2);
+		
+		String name;
+		name = "Eric Rasmussen";
+		String address;
+		address = "13372 Wendover Terrace";
+		String City;
+		City = "San Diego";
+		String zip;
+		zip = "92130";
+		System.out.println(name);
+		System.out.println(address);
+		System.out.println(City);
+		System.out.println(zip);
+	
+	}
+}
