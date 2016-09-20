5e230a3ad5f48b9ad2269e567e6d1318d4a6aa55
RasmussenEric <ericrasmussen17@yahoo.com>
2016-09-20 11:46:38 -0700

    Finished Lesson_04

---
+++ APLesson_04/Card.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Card
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		Card form = new Card();
+		
+		System.out.println("Enter your first name:");
+		String left2 = kb.nextLine();
+		
+		System.out.println("Enter your last name:");
+		String right2 = kb.nextLine();
+		
+		System.out.println("Enter your title:");
+		String left3 = kb.nextLine();
+		
+		System.out.println("Enter the school name:");
+		String left1 = kb.nextLine();
+		
+		System.out.println("Enter the school year:");
+		String right1 = kb.nextLine();
+		
+		System.out.println("Enter your subject:");
+		String right3 = kb.nextLine();
+		
+		String line1 = left1 + "           " + right1;
+		String line2 = left2 + "         " + right2;
+		String line3 = left3 + "  " + right3;
+		
+		System.out.println("\n***********************************");
+		form.format("*  " + line1);
+		form.format("*       " + line2);
+		form.format("*       " + line3);
+		System.out.println("\n***********************************");
+	}
+	public void format(String line)
+	{
+		System.out.printf("\n%35s", line + " *");
+	}
+}
+
--- APLesson_04/Recipt.java
+++ APLesson_04/Recipt.java
@@ -9,53 +9,61 @@ public class Recipt
 		Scanner keyboard = new Scanner(System.in);
 		
 		
-		System.out.format("Please enter item 1:");
+		System.out.println("Please enter item 1:");
 		String item1 = keyboard.nextLine();
 		
-		System.out.format("Please enter the price:");
+		System.out.println("Please enter the price:");
 		Double price1 = keyboard.nextDouble();
 		
 		
 		keyboard.nextLine();
-		System.out.format("Please enter item 2:");
+		System.out.println("Please enter item 2:");
 		String item2 = keyboard.nextLine();
 		
-		System.out.format("Please enter the price:");
+		System.out.println("Please enter the price:");
 		Double price2 = keyboard.nextDouble();
 		
 		
 		keyboard.nextLine();
-		System.out.format("Please enter item 3:");
+		System.out.println("Please enter item 3:");
 		String item3 = keyboard.nextLine();
 		
-		System.out.format("Please enter the price:");
+		System.out.println("Please enter the price:");
 		Double price3 = keyboard.nextDouble();
 		
 		Double total1 = (price1 + price2 + price3);
-		Double total2 = (total1 * .07);
+		Double total2 = (total1 * .08);
 		Double total3 = (total1 + total2);
 		
-		String name1 = "subtotal";
-		String name2 = "tax";
-		String name3 = "total";
+		String name1 = "*        Subtotal:";
+		String name2 = "*             Tax:";
+		String name3 = "*           Total:";
 		
-		form.recipt(item1, price1);
-		form.recipt(item2, price2);
-		form.recipt(item3, price3);
+		System.out.println("\n<<<<<<<<<<<<___Receipt___>>>>>>>>>>>>");
+		
+		form.recipt("*  " + item1, price1);
+		form.recipt("*     " + item2, price2);
+		form.recipt("*             " + item3, price3);
+		
+		System.out.println("\n");
 		
 		form.recipt2(name1, total1);
 		form.recipt2(name2, total2);
 		form.recipt2(name3, total3);
+		
+		System.out.println("\n______________________________");
+		System.out.println("* Thank you for your support *");
 	}
 	
 	public void recipt(String item, double price)
 	{
 		
-		System.out.printf("\n%15s  %10.2f", item, price);
+		System.out.printf("\n%18s  %4.2f", item + ".......", price);
 		
 	}	
 	public void recipt2(String name, double total)
 	{
-		System.out.printf("\n%15s  %10.2f", name, total);
+		System.out.printf("\n%18s  %4.2f", name + ".......", total);
 	}
+	
 }
