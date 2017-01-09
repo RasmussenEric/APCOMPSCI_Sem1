6ccc1a66bbc7e5b8f4e17931109a47475abb5d1f
RasmussenEric <ericrasmussen17@yahoo.com>
2017-01-09 12:11:09 -0800

    Lesson 12

---
+++ APLesson_12/MPHRunner.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class MPHRunner
+{
+	static int D;
+	static int H;
+	static int M;
+	
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Enter distance:");
+		D = kb.nextInt();
+		
+		System.out.println("Enter hours:");
+		H = kb.nextInt();
+		
+		System.out.println("Enter minutes:");
+		M = kb.nextInt();
+		
+		//MilesPerHour object = new MilesPerHour();
+		
+		MilesPerHour object2 = new MilesPerHour(D, H, M);
+		
+		//System.out.println(object.getD() + " miles in " + (object.getH() + (object.getM() /60)) + " = " + object.getMPH() + " mph");
+		
+		object2.setValues(D, H, M);
+		
+		System.out.println(object2.getD() + " miles in " + (object2.getH() + (object2.getM()/60)) + " hours = " + object2.getMPH() + " mph");
+		
+		
+	}
+	
+	
+}
--- APLesson_12/MilesPerHour.java
+++ APLesson_12/MilesPerHour.java
@@ -1,8 +1,57 @@
 import static java.lang.Math.*;
+import java.util.Scanner;
 public class MilesPerHour
 {
 	private int distance, hours, minutes;
+	private double mph;
+	//static int D;
+	//static int H;
+	//static int M;
 	
+	public MilesPerHour()
+	{
+		distance = 0;
+		hours = 0;
+		minutes = 0;
+		mph = 0;
+	}
 	
+	public MilesPerHour(int D, int H, int M)
+	{
+		distance = D;
+		hours = H;
+		minutes = M;
+		mph = 0;
+	}
+	
+	public void setValues(int D, int H, int M)
+	{
+		distance = D;
+		hours = H;
+		minutes = M;
+		mph = 0;
+	}
+	
+	public double getMPH()
+	{
+		return mph = Math.round(distance / (hours + (minutes / 60)));
+	}
+	
+	public int getD()
+	{
+		return distance;
+	}
+	
+	public int getH()
+	{
+		return hours;
+	}
+	
+	public int getM()
+	{
+		return minutes;
+	}
+	
+
 	
 }
