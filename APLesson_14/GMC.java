a659efa534f2151319f338b6be005546d0601254
RasmussenEric <ericrasmussen17@yahoo.com>
2017-03-02 12:12:51 -0800

    14

---
+++ APLesson_14/GMC.java
@@ -0,0 +1,23 @@
+public class GMC
+{
+	
+	public int getID()
+	{
+		return (int)(Math.random() * 1000000) + 1;
+		
+	}
+	
+	public void move(int x, int y)
+	{
+		
+		int coordinatex = x;
+		int coordinatey = y;
+	}
+	
+	public double[] getLoc()
+	{
+		
+		double[] location = new double[coordinatex,coordinatey]() ;
+		return location;
+	}
+}
---
+++ APLesson_14/Toyota.java
@@ -0,0 +1,6 @@
+public class Toyota
+{
+	
+	String[] temp = new String[](c.split());
+	
+}
